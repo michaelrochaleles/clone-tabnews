cd59d40363a5f71f665afb6c11d4299c49182fd3
Michael Rocha Leles <43224508+michaelrochaleles@users.noreply.github.com>
2025-12-08 09:57:52 +0000

    Alteração de impacto com minha esposa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Eu amo a minha família!</h1>
+    return <h1>Eu amo a minha família! Você é a minha escolha, meu amor! Me abrace e beije 🥰</h1>
 }
 
 export default Home;
